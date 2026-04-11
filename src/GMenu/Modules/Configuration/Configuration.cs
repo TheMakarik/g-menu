a598f66d19470fd8a067d33cb698b3dd2f1f33c9
themakarik <themakarik@gmail.com>
2026-04-11 14:54:31 +0700

    Configuration updating become easier

--- src/GMenu/Modules/Configuration/Configuration.cs
+++ src/GMenu/Modules/Configuration/Configuration.cs
@@ -75,7 +75,7 @@ public class Configuration(ILogger logger) : IConfiguration
                 logger.Error(e, "Something go wrong during configuration update");
                 throw;
             }
-            logger.Information("Configuration key ({key}) will be  changed in {path}", args.PropertyName, StaticConfiguration.ConfigurationPath);
+            logger.Information("Configuration key ({key}) will be changed in {path}", args.PropertyName, StaticConfiguration.ConfigurationPath);
             
         };
         _ = HandleJSONChangingAsync(_channel.Reader);
@@ -88,79 +88,15 @@ public class Configuration(ILogger logger) : IConfiguration
             await _semaphoreSlim.WaitAsync();
             try
             {
-                if (propertyName is null)
-                    return;
-
-                var json = File.Open(StaticConfiguration.ConfigurationPath, FileMode.Open);
-                var jsonNode = await JsonNode.ParseAsync(json);
-        
-                if (IsArrayValueUpdated(propertyName, out var index, out var name))
-                {
-                    var newValue = ((IList)_configuration!.GetType()
-                        .GetProperty(name)?.GetValue(_configuration)!)[index];
-            
-                    var arrayNode = jsonNode![name]?.AsArray();
-                    if (arrayNode != null)
-                    {
-                        //May have some problems with NativeAOT so check that all possible types of new object is primitives or has System.Diagnostic.CodeAnalyses.DynamicallyAccessedMembersAttribute
-                        var newNode = JsonSerializer.SerializeToNode(newValue);
-                        arrayNode[index] = newNode;
-                    }
-                }
-                else
-                {
-                    var newValue = _configuration!.GetType()
-                        .GetProperty(propertyName)?.GetValue(_configuration)!;
-            
-                    //May have some problems with NativeAOT so check that all possible types of new object is primitives or has System.Diagnostic.CodeAnalyses.DynamicallyAccessedMembersAttribute
-                    var newNode = JsonSerializer.SerializeToNode(newValue);
-                    jsonNode![propertyName] = newNode;
-                }
-            
-                var options = new JsonSerializerOptions
-                {
-                    WriteIndented = true
-                };
-            
-                var updatedJson = jsonNode!.ToJsonString(options);
-                await File.WriteAllTextAsync(StaticConfiguration.ConfigurationPath, updatedJson);
+                await using var json = File.Open(StaticConfiguration.ConfigurationPath, FileMode.Open);
+                await JsonSerializer.SerializeAsync(json, _configuration, ObservableConfigurationSerializerContext.Default.Options);
+                logger.Debug("Configuration key ({key}) was fully changed in {path}", propertyName, StaticConfiguration.ConfigurationPath);
             }
             finally
             {
                 _semaphoreSlim.Release();
             }
-           
-        }
-    }
 
-    private bool IsArrayValueUpdated(string? propertyName, out int index, [NotNullWhen(true)] out string? name)
-    {
-        index = 0;
-        name = null;
-        if (propertyName is null)
-            return false;
-        
-        if (!propertyName.EndsWith("]"))
-            return false;
-        
-        //Minus one because indexes are zero based and minus one again because we have just checked last charecter
-        var startI = propertyName.Length - 2;
-        for (var i = startI; i >= 0; i--)
-        {
-            if (propertyName[i] == '[')
-            {
-                if (i == startI)
-                    return false;
-                
-                index = i - 1;
-                name = propertyName[..i];
-                return true;
-                
-            }
-            if (!int.TryParse(propertyName[i].ToString(), out var _))
-                return false;
         }
-        return false;
-
     }
 }
